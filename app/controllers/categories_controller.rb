06cda166755099cb66fabf951e06ca79256a8d4a
tiennh1995 <nguyen.huu.tien@framgia.com>
2016-11-30 13:53:42 +0700

    Fix search book

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -2,6 +2,8 @@ class CategoriesController < ApplicationController
   load_and_authorize_resource
 
   def show
+    @books = (params[:type].nil? || params[:type] == "all") ? @books :
+      @books.send("#{params[:type]}", current_user.id)
     @q = @category.books.ransack params[:q]
     @books = @q.result.page(params[:page]).per Settings.per_page
   end
